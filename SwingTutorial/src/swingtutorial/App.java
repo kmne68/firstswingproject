38c0d5dd8b662ba79928a452c53d52c2590f0f9d
Keith Emery <kmne68@gmail.com>
2016-08-01 14:13:46 -0500

    Worked through first coding tutorial.

--- SwingTutorial/src/swingtutorial/App.java
+++ SwingTutorial/src/swingtutorial/App.java
@@ -5,18 +5,34 @@
  */
 package swingtutorial;
 
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
+
 /**
  * Following tutorial at:
  * https://www.udemy.com/java-swing-complete/?couponCode=Y5Y2PEYD
  * @author kemery
  */
-public class SwingTutorial {
+public class App {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        SwingUtilities.invokeLater(new Runnable() {
+
+            @Override
+            public void run() {
+
+                JFrame frame = new JFrame("Hello World");
+
+                frame.setSize(600, 500);
+                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                frame.setVisible(true);   }
+        });
+        
+
     }
     
 }
