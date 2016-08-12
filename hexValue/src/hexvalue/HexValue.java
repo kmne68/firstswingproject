1869f4fde507e9086068f1954a2ec08a60835c0a
Keith Emery <kmne68@gmail.com>
2016-08-12 18:34:14 -0500

    Switch program example.

---
+++ hexValue/src/hexvalue/HexValue.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hexvalue;
+
+/**
+ *
+ * @author kemery
+ */
+public class HexValue {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    static int decimalValue;
+    static char myValue;
+    
+    public static void main(String[] args) {
+        
+        System.out.println("The decimal value of the hex number is: " + hexValue(myValue));
+              
+    }
+    
+    
+    public static int hexValue(char value) {
+        
+        return decimalValue;
+    }
+}
