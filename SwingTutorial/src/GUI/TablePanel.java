1145db35c64f1aed3fe79b0b55b68ef44a643c1a
kmne68 <kmne68@gmail.com>
2016-08-28 23:18:52 -0500

    Added popup menu to table.

--- SwingTutorial/src/GUI/TablePanel.java
+++ SwingTutorial/src/GUI/TablePanel.java
@@ -8,8 +8,12 @@ package GUI;
 import Model.Person;
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.util.List;
+import javax.swing.JMenuItem;
 import javax.swing.JPanel;
+import javax.swing.JPopupMenu;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 
@@ -21,14 +25,31 @@ public class TablePanel extends JPanel {
     
     private JTable table;
     private PersonTableModel tableModel;
-    
+    private JPopupMenu popup;
     
     public TablePanel () {
         
         tableModel = new PersonTableModel();
         table = new JTable(tableModel);
+        popup = new JPopupMenu();
         
-        table.setGridColor(Color.black);
+        JMenuItem removeItem = new JMenuItem("Delete row");
+        popup.add(removeItem);
+        
+        
+        
+        table.setGridColor(Color.black);           
+        table.addMouseListener(new MouseAdapter() {
+        
+            @Override
+            public void mousePressed(MouseEvent me) {
+               if(me.getButton() == MouseEvent.BUTTON3) {
+                   popup.show(table, me.getX(), me.getY());
+               }
+              //  super.mousePressed(me); 
+            }
+    });
+      
         
         setLayout(new BorderLayout());
                 
