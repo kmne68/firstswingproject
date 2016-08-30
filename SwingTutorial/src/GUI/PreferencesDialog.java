fa4f86229f6aa4b84c0616d9ceac308c59697833
Keith Emery <kmne68@gmail.com>
2016-08-30 13:04:23 -0500

    Added port selection spinner to preferences dialog.

--- SwingTutorial/src/GUI/PreferencesDialog.java
+++ SwingTutorial/src/GUI/PreferencesDialog.java
@@ -5,8 +5,16 @@
  */
 package GUI;
 
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JSpinner;
+import javax.swing.SpinnerNumberModel;
 
 /**
  *
@@ -14,10 +22,68 @@ import javax.swing.JFrame;
  */
 public class PreferencesDialog extends JDialog {
     
+    private JButton btn_ok;
+    private JButton btn_cancel;
+    private JSpinner portSpinner;
+    private SpinnerNumberModel spinnerModel;
+    
     public PreferencesDialog(JFrame parent) {
         super(parent, "Preferences", false);
         
+        btn_ok = new JButton("OK");
+        btn_cancel = new JButton("Cancel");
+        
+        spinnerModel = new SpinnerNumberModel(3306, 0, 9999, 1);
+        portSpinner = new JSpinner(spinnerModel);
+        
+        setLayout(new GridBagLayout());
+        
+        GridBagConstraints gc = new GridBagConstraints();
+        
+        gc.gridy = 0;
+        gc.weightx = 1;
+        gc.weighty = 1;
+        gc.fill = GridBagConstraints.NONE;
+        
+        gc.gridx = 0;
+        
+        add(new JLabel("Port: "), gc);
+        gc.gridx++;
+        add(portSpinner, gc);
+        
+        // next row
+        gc.gridy++;
+        gc.gridx = 0;
+        add(btn_ok, gc);
+        
+        gc.gridx++;
+        add(btn_cancel, gc);
+        
+        btn_ok.addActionListener(new ActionListener() {
+           
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                
+                Integer value = (Integer)portSpinner.getValue();
+                
+                System.out.println(value);
+                setVisible(false);
+            }
+        });
+        
+        
+        btn_cancel.addActionListener(new ActionListener() {
+           
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                
+                setVisible(false);
+            }
+        });
+        
+        
         setSize(400, 300);
+        setLocationRelativeTo(parent);
     } 
     
 }
