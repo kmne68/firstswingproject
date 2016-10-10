df1d8f3b6adf3a13ef88d15349383c75da904b27
Keith Emery <kmne68@gmail.com>
2016-10-10 13:03:15 -0500

    Add check box to data table.

--- SwingTutorial/src/GUI/PersonTableModel.java
+++ SwingTutorial/src/GUI/PersonTableModel.java
@@ -36,6 +36,8 @@ public class PersonTableModel extends AbstractTableModel {
         switch(column) {
             case 1:
                 return true;
+            case 5:
+                return true;
             default:
                 return false;
         }
@@ -54,12 +56,39 @@ public class PersonTableModel extends AbstractTableModel {
             case 1:
                 person.setName((String) value);
                 break;
+            case 5:
+                person.setUsCitizen((Boolean)value);
             default:
                 return;
         }
     }
+
+    @Override
+    public Class<?> getColumnClass(int columnIndex) {
+        
+        switch(columnIndex) {
+            case 0:
+                return Integer.class;
+            case 1:
+                return String.class;
+            case 2:
+                return String.class;
+            case 3:
+                return String.class;
+            case 4:
+                return String.class;
+            case 5:
+                return Boolean.class;
+            case 6:
+                return String.class;
+            case 7:
+                return String.class;
+            default:
+                return null; 
+        }
+    }
     
-    
+
     
     
     
